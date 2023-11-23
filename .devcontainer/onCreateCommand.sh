d0ece5e17024394a45d743337413aa9625a78603
Bill Buffet <lonrun@126.com>
2023-11-23 16:54:06 +0800

    Update onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Assumes the gh CLI is present in the default Codespaces image.
 export sparrowCliVersion=bc184bb8
 curl https://antsys-sparrow-data.cn-shanghai-alipay-office.oss-alipay.aliyuncs.com/sparrow/public/sparrow-cli/sparrow-cli-${sparrowCliVersion}.linux.tar.gz -o sparrow-cli.tar.gz && tar -zxvf sparrow-cli.tar.gz && rm sparrow-cli.tar.gz
 
 echo "current sparrow cli dir is sparrow-cli-${sparrowCliVersion}"
-  if [ -d sparrow-cli-${sparrowCliVersion} ]; then
-   echo "change sparrow cli dir"
-   mv sparrow-cli-${sparrowCliVersion} sparrow-cli
-  fi
+if [ -d sparrow-cli-${sparrowCliVersion} ]; then
+ echo "change sparrow cli dir"
+ mv sparrow-cli-${sparrowCliVersion} sparrow-cli
+fi
 
-  export PATH=${PWD}/sparrow-cli:$PATH
+export PATH=${PWD}/sparrow-cli:$PATH
+echo $PATH
 
-  sparrow version info
+which sparrow
+sparrow version info
