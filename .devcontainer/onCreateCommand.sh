d64bc549a4e2f186f8c0b03244a3754e1e147d04
Bill Buffet <lonrun@126.com>
2023-11-24 10:03:58 +0800

    Update onCreateCommand.sh

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
 export sparrowCliVersion=bc184bb8
-curl https://antsys-sparrow-data.cn-shanghai-alipay-office.oss-alipay.aliyuncs.com/sparrow/public/sparrow-cli/sparrow-cli-${sparrowCliVersion}.linux.tar.gz -o sparrow-cli.tar.gz && tar -zxvf sparrow-cli.tar.gz && rm sparrow-cli.tar.gz
-
-echo "current sparrow cli dir is sparrow-cli-${sparrowCliVersion}"
-if [ -d sparrow-cli-${sparrowCliVersion} ]; then
- echo "change sparrow cli dir"
- mv sparrow-cli-${sparrowCliVersion} sparrow-cli
-fi
-
-export PATH=${PWD}/sparrow-cli:$PATH
-echo $PATH
-
-which sparrow
-sparrow version info
